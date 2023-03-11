a77baaaed3494274df3f156f737d4cfc16a4d1a5
LukMinav <127608423+LukMinav@users.noreply.github.com>
2023-03-11 11:12:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-bzw4k8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-bzw4k8)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function MiComponente() {
+  return (
+    <div>
+      <h1>Mi Título</h1>
+      <button onClick={() => console.log('Haz clic en el botón')}>Haz clic aquí</button>
+    </div>
+  );
+}
